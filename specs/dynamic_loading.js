9d848d6e8afeed44dff66cedb1710ee5e1b56983
voyagerfaraway <68065551+voyagerfaraway@users.noreply.github.com>
2020-07-13 16:48:40 +0300

    Update dynamic_loading.js
    new tests added - check the loading message and loading image to have appeared

--- specs/dynamic_loading.js
+++ specs/dynamic_loading.js
@@ -1,11 +1,39 @@
 const dynamicLoadingPage = require("../page_objects/dynamicLoadingPage");
 describe('dynamic loading pages', function(){
+    it('open the first link, click start and confirm loading message to have appeared', function(){ 
+        dynamicLoadingPage.getFirst();
+        dynamicLoadingPage.clickStart();
+
+        expect(dynamicLoadingPage.getLoadingText()).toEqual('Loading...');
+    });
+
+    it('open the first link, click start and confirm loading image to have appeared', function(){ 
+        dynamicLoadingPage.getFirst();
+        dynamicLoadingPage.clickStart();
+
+        expect(dynamicLoadingPage.getImage().isDisplayed()).toBe(true);
+    });
+
     it('open the first link, click start and confirm hello world to have appeared', function(){ 
         dynamicLoadingPage.getFirst();
         dynamicLoadingPage.clickStart();
         dynamicLoadingPage.waitToAppear();
 
         expect(dynamicLoadingPage.getHelloWorldText()).toEqual('Hello World!');
+    });
+
+    it('open the second link, click start and confirm loading message to have appeared', function(){ 
+        dynamicLoadingPage.getSecond();
+        dynamicLoadingPage.clickStart();
+
+        expect(dynamicLoadingPage.getLoadingText()).toEqual('Loading...');
+    });
+
+    it('open the second link, click start and confirm loading image to have appeared', function(){ 
+        dynamicLoadingPage.getSecond();
+        dynamicLoadingPage.clickStart();
+
+        expect(dynamicLoadingPage.getImage().isDisplayed()).toBe(true);
     });
 
     it('open the second link, click start and confirm hello world to have appeared', function(){       
